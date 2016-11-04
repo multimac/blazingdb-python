a86bef93048ffc08d35a7780c08d4172aae1818c
Emilse <emilse@blazingdb.com>
2016-11-04 19:09:24 -0300

    Update

--- test/migration_test.py
+++ test/migration_test.py
@@ -3,19 +3,20 @@ import psycopg2 as postgres
 
 # Blazing Connection
 bl = BlazingPyConnector('localhost','test@blazingdb.com','tester','nada')
-con = bl.connect()
-
+print "Coneccion"
 # Postgresql Connection
-pg = postgres.connect(host='localhost',dbname='postgres',user='postgres',password='postgres')
-
+pg = postgres.connect(host='localhost',dbname='nada',user='postgres',password='postgres')
+print "Dos"
 migrator = BlazingETL(pg, bl)
+print "Tres"
 migrator.migrate(
-    create_tables=False,                                            # Specify if it's needed to create the tables in the migration
-    path='/home/user/datasets/',                                    # Specify where will the migration data files stored
+    create_tables=False,                                             # Specify if it's needed to create the tables in the migration
+    path='/home/user/OneModel/',                                    # Specify where will the migration data files stored
     blazing_path='/opt/blazing/disk1/blazing/blazing-uploads/',     # Specify in case it's wanted to copy the data to BlazingDB uploads folder
     chunk_size=100000,                                              # Specify the size of the chunks of data to migrate
     export_data_from_origin=True,                                   # Turn to False in case you already have the and only need to load them into BlazingDb
     copy_data_to_blazing=False,                                     # Turn to True in case you want to copy the data to BlazingDB uploads folder
     load_data_into_blazing=True,                                    # Turn to False in case you want to migrate only the structure and not the data
-    file_extension='.csv'                                           # Specify the data files extension
+    file_extension='.dat',                                          # Specify the data files extension
+    by_stream=True                                                  # Change to false if you need the migration files generated
 )
