4edd2ad534cc102489a04374f06d082bf6cbe763
David Symons <david.symons@onemodel.co>
2017-04-05 16:57:28 +1000

    Remove all unnecessary files

--- test/import_test.py
+++
@@ -1,8 +0,0 @@
-from blazingdb import BlazingPyConnector, BlazingImporter
-
-# Blazing Connection
-bl = BlazingPyConnector('localhost','test@blazingdb.com','tester','test')
-print "Connection"
-
-importer = BlazingImporter(bl)
-importer.file_import()
