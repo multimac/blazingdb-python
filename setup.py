8c0f278a473b074aaea9889c48f5ed9b6b4e4b5f
David Symons <david.symons@onemodel.co>
2017-04-07 10:59:25 +1000

    Bump to version 1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="blazingdb",
-    version="1.0.6",
+    version="1.1.0",
 
     description=" ".join([
         "Contains the relevant classes for connecting to, and",
