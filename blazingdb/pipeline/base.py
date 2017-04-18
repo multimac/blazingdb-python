090270d922ca30ff64c8d77cf18e4877d95079b1
David Symons <david.symons@onemodel.co>
2017-04-18 13:01:49 +1000

    Fix virtual method arguments

--- blazingdb/pipeline/base.py
+++ blazingdb/pipeline/base.py
@@ -5,10 +5,10 @@ Defines the base stage class for use during data migration
 class BaseStage(object):
     """ Base class for all pipeline stages """
 
-    async def begin_import(data):
+    async def begin_import(self, data):
         """ Called before data begins being piped through the pipeline """
         pass
 
-    async def end_import(data):
+    async def end_import(self, data):
         """ Called after before data begins being piped through the pipeline """
         pass
