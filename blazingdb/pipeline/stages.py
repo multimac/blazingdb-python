28ae2b219dccae9722adfd9d99a2dc3df1e3c592
David Symons <david.symons@onemodel.co>
2017-05-08 10:36:59 +1000

    Fix invalid string.format

--- blazingdb/pipeline/stages.py
+++ blazingdb/pipeline/stages.py
@@ -76,9 +76,9 @@ class CustomQueryStage(base.BaseStage):
 
     async def _perform_query(self, data):
         connector = data["connector"]
-        formatted_query = self.query.format({
-            "table": data["dest_table"]
-        })
+        formatted_query = self.query.format(
+            table = data["dest_table"]
+        )
 
         results = await connector.query(formatted_query)
 
