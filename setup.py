6db6a8bbd87399bf3f1043f2d8d050e77fffd8b3
David Symons <david.symons@onemodel.co>
2017-05-15 10:51:01 +1000

    Version 1.3.0.beta3.dev1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="blazingdb",
-    version="1.3.0.beta2",
+    version="1.3.0.beta3.dev1",
 
     description=" ".join([
         "Contains the relevant classes for connecting to, and",
