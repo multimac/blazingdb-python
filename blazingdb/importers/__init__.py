753f44bf7e0ba771748f5852c9d4302c2efc6d79
David Symons <david.symons@onemodel.co>
2017-05-24 11:57:39 +1000

    Create SkipImporter

--- blazingdb/importers/__init__.py
+++ blazingdb/importers/__init__.py
@@ -3,6 +3,7 @@ Package containing all the different importers to load data into BlazingDB
 """
 
 from .chunking import ChunkingImporter
+from .skip import SkipImporter
 from .stream import StreamImporter
 
-__all__ = ["base", "chunking", "processor", "stream"]
+__all__ = ["base", "chunking", "processor", "skip", "stream"]
---
+++ blazingdb/importers/skip.py
@@ -0,0 +1,20 @@
+"""
+Defines the SkipImporter class which is a basic implementation of BaseImport that doesn't
+import any data.
+"""
+
+import logging
+
+from . import base
+
+
+class SkipImporter(base.BaseImporter):  # pylint: disable=too-few-public-methods
+    """ Skips importing any data into Blazing """
+
+    def __init__(self, **kwargs):
+        super(SkipImporter, self).__init__(**kwargs)
+        self.logger = logging.getLogger(__name__)
+
+    async def load(self, connector, data):
+        """ Logs out which table would have been imported int Blazing """
+        self.logger.info("Skipping import of table %s", data["dest_table"])
