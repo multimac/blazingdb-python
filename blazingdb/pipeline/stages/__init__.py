6821737f78781a6406950a3960016e4f2dc6f092
David Symons <david.symons@onemodel.co>
2017-06-15 10:06:34 +1000

    Update stages __init__.py

--- blazingdb/pipeline/stages/__init__.py
+++ blazingdb/pipeline/stages/__init__.py
@@ -10,8 +10,9 @@ from .base import BaseStage, When
 
 from .batch import ByteBatchStage, RowBatchStage
 from .custom import CustomActionStage, CustomCommandStage, CustomQueryStage
-from .database import CreateTableStage, DropTableStage, SourceComparisonStage, TruncateTableStage
-from .misc import DelayStage, PromptInputStage, PrefixTableStage, SkipImportStage
+from .database import CreateTableStage, DropTableStage, PostImportHackStage
+from .database import SourceComparisonStage, TruncateTableStage
+from .misc import DelayStage, PromptInputStage, SkipImportStage
 from .sources import FilterColumnsStage, JumbleDataStage, LimitImportStage
 from .stream import StreamGenerationStage
 
