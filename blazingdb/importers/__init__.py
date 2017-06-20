cd358de8c44db93939114365dd4bd8c8c7868671
David Symons <david.symons@onemodel.co>
2017-06-20 15:32:18 +1000

    Remove old SkipImporter reference

--- blazingdb/importers/__init__.py
+++ blazingdb/importers/__init__.py
@@ -4,7 +4,6 @@ Package containing all the different importers used to load data into BlazingDB
 
 from .base import BaseImporter, RowFormat
 from .chunking import ChunkingImporter
-from .skip import SkipImporter
 from .stream import StreamImporter
 
-__all__ = ["base", "chunking", "skip", "stream"]
+__all__ = ["base", "chunking", "stream"]
