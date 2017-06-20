e63eeac3a690c5d210e7672b961af32671d7a1f5
David Symons <david.symons@onemodel.co>
2017-06-20 10:59:49 +1000

    Update misc stages to handle messages

--- blazingdb/pipeline/stages/misc.py
+++ blazingdb/pipeline/stages/misc.py
@@ -9,7 +9,9 @@ import asyncio
 import fnmatch
 
 from blazingdb import exceptions
+
 from . import base, custom
+from .. import messages
 
 
 # pragma pylint: disable=too-few-public-methods
@@ -18,7 +20,7 @@ class DelayStage(custom.CustomActionStage):
     """ Pauses the pipeline before / after importing data """
 
     def __init__(self, delay, **kwargs):
-        super(DelayStage, self).__init__(self._delay, **kwargs)
+        super(DelayStage, self).__init__(self._delay, [messages.ImportTableMessage], **kwargs)
         self.prompt = kwargs.get("prompt", "Waiting for input...")
         self.delay = delay
 
@@ -30,7 +32,7 @@ class PromptInputStage(custom.CustomActionStage):
     """ Prompts for user input to continue before / after importing data """
 
     def __init__(self, **kwargs):
-        super(PromptInputStage, self).__init__(self._prompt, **kwargs)
+        super(PromptInputStage, self).__init__(self._prompt, [messages.ImportTableMessage], **kwargs)
         self.prompt = kwargs.get("prompt", "Waiting for input...")
 
     async def _prompt(self, data):  # pylint: disable=unused-argument
@@ -40,16 +42,18 @@ class PromptInputStage(custom.CustomActionStage):
 class SkipImportStage(base.BaseStage):
     """ Skips the import if this stage is reached """
 
-    async def before(self, data):  # pylint: disable=unused-argument
-        """ Raises a SkipImportException to skip the import of data """
-        raise exceptions.SkipImportException()
+    def __init__(self):
+        super(SkipImportStage, self).__init__(messages.ImportTableMessage)
+
+    async def process(self, message):  # pylint: disable=unused-argument
+        """ Ignores the message to prevent it being forwarded on """
 
 
 class SkipTableStage(base.BaseStage):
     """ Skips tables based on a given set of inclusions and exclusions """
 
     def __init__(self, included=None, excluded=None):
-        super(SkipTableStage, self).__init__()
+        super(SkipTableStage, self).__init__(messages.ImportTableMessage)
 
         self.included = included
         self.excluded = excluded
@@ -69,7 +73,7 @@ class SkipTableStage(base.BaseStage):
 
         return True
 
-    async def before(self, data):
-        """ Raises a SkipImportException if the table should be filtered """
-        if self._filter_table(data["src_table"]):
-            raise exceptions.SkipImportException()
+    async def process(self, message):
+        """ Only calls message.forward if the message isn't filtered """
+        if not self._filter_table(message.src_table):
+            await message.forward()
