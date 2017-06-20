fa6cbc871458d349e2087f06ab06e687f4841a1e
David Symons <david.symons@onemodel.co>
2017-06-20 10:29:02 +1000

    Update Message, and add get_parent method

--- blazingdb/pipeline/messages.py
+++ blazingdb/pipeline/messages.py
@@ -2,38 +2,53 @@
 Defines the core message classes used in the pipeline
 """
 
-import itertools
-import operator
+import copy
 
 
-class Message(tuple):
+class Message(object):
     """ Base class used for all messages passed in the pipeline """
-    __slots__ = ()
-
-    msg_id = property(operator.itemgetter(0))
-    stages = property(operator.itemgetter(1))
-
-    msgs = property(operator.itemgetter(2))
-
-    def __new__(cls, msg_id=0):
-        return tuple.__new__(cls, (msg_id, None, []))
 
     def __init__(self):
-        super(Message, self).__init__()
-        self.msgs.append(self)
+        self.msg_id = id(self)
+
+        self.parent = None
+        self.stages = None
+        self.msgs = []
+
+    def _rebuild(self, parent, stages):
+        self.msg_id = id(self)
+
+        self.parent = parent
+        self.stages = stages.copy()
 
     def get_messages(self, types):
-        """ Retrieves all messages of the given types """
-        arg_list = itertools.product(self.msgs, types)
-        check_type = lambda args: isinstance(*args)
+        """ Retrieves all contained messages of the given types """
+        check_type = lambda msg: isinstance(msg, types)
+        return set(filter(check_type, self.msgs))
 
-        return set(filter(check_type, arg_list))
+    def get_parent(self, types):
+        """ Retrieves the closest parent of one of the given types """
+        current = self.parent
+        while current is not None:
+            if isinstance(current, types):
+                return current
 
-    async def forward(self, msg=None):
+            current = current.parent
+
+        return None
+
+    async def forward(self, msg=None, **updates):
         """ Forwards the given message on to the next stage in the pipeline """
-        msg = msg if msg is not None else self
+        if msg is None:
+            msg = copy.copy(self)
+        elif updates:
+            msg = copy.copy(msg)
 
-        msg.stages = self.stages.copy()
+        for key, value in updates.items():
+            setattr(self, key, value)
+
+        # pragma pylint: disable=protected-access
+        msg._rebuild(self, self.stages)
         next_stage = msg.stages.popleft()
 
         await next_stage.receive(msg)
@@ -48,6 +63,13 @@ class ImportTableMessage(Message):
         self.dest_table = dest_table
         self.destination = destination
 
+
 class LoadDataMessage(Message):
-    def __init__(self, destination, table, data):
-        super()
+    """ Message defining a batch of data to be imported """
+    def __init__(self, table, source, data, final=True):  # pylint: disable=too-many-arguments
+        super(LoadDataMessage, self).__init__()
+        self.table = table
+        self.source = source
+        self.data = data
+
+        self.final = final
