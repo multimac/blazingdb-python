7f443913b51c970984d226130b8a54280131bada
David Symons <david.symons@onemodel.co>
2017-06-20 14:41:24 +1000

    Update misc stages to handle packets

--- blazingdb/pipeline/stages/misc.py
+++ blazingdb/pipeline/stages/misc.py
@@ -20,7 +20,7 @@ class DelayStage(custom.CustomActionStage):
     """ Pauses the pipeline before / after importing data """
 
     def __init__(self, delay, **kwargs):
-        super(DelayStage, self).__init__(self._delay, [messages.ImportTableMessage], **kwargs)
+        super(DelayStage, self).__init__(self._delay, **kwargs)
         self.prompt = kwargs.get("prompt", "Waiting for input...")
         self.delay = delay
 
@@ -32,7 +32,7 @@ class PromptInputStage(custom.CustomActionStage):
     """ Prompts for user input to continue before / after importing data """
 
     def __init__(self, **kwargs):
-        super(PromptInputStage, self).__init__(self._prompt, [messages.ImportTableMessage], **kwargs)
+        super(PromptInputStage, self).__init__(self._prompt, **kwargs)
         self.prompt = kwargs.get("prompt", "Waiting for input...")
 
     async def _prompt(self, data):  # pylint: disable=unused-argument
@@ -43,7 +43,7 @@ class SkipImportStage(base.BaseStage):
     """ Skips the import if this stage is reached """
 
     def __init__(self):
-        super(SkipImportStage, self).__init__(messages.ImportTableMessage)
+        super(SkipImportStage, self).__init__(messages.ImportTablePacket)
 
     async def process(self, message):  # pylint: disable=unused-argument
         """ Ignores the message to prevent it being forwarded on """
@@ -53,7 +53,7 @@ class SkipTableStage(base.BaseStage):
     """ Skips tables based on a given set of inclusions and exclusions """
 
     def __init__(self, included=None, excluded=None):
-        super(SkipTableStage, self).__init__(messages.ImportTableMessage)
+        super(SkipTableStage, self).__init__(messages.ImportTablePacket)
 
         self.included = included
         self.excluded = excluded
