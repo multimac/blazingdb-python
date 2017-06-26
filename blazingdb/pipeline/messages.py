a7587e9b89b4d2fe3d8a25e35c4c2f3b9b402c20
David Symons <david.symons@onemodel.co>
2017-06-26 16:10:02 +1000

    Fix incorrect assignment to dict in Message

--- blazingdb/pipeline/messages.py
+++ blazingdb/pipeline/messages.py
@@ -33,7 +33,7 @@ class Message(object):
 
     def __getstate__(self):
         state = self.__dict__.copy()
-        state.parent = self.get_initial_message()
+        state["parent"] = self.get_initial_message()
 
         del state["system"]
         return state
