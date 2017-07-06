77f5c6142af9be2924794ade1480ff529992b828
David Symons <david.symons@onemodel.co>
2017-07-06 16:10:04 -0500

    Fix up System.shutdown

--- blazingdb/pipeline/system.py
+++ blazingdb/pipeline/system.py
@@ -25,15 +25,30 @@ class System(object):
 
         self.loop = loop
         self.stages = list(stages) + [System.BlackholeStage()]
+        self.tasks = set()
 
     async def _process_message(self, message):
         await self.stages[message.stage_idx].receive(message)
+        self.tasks.remove(asyncio.Task.current_task())
 
     async def enqueue(self, message):
         """ Queues a given message to be processed """
         message.system = self
 
-        asyncio.ensure_future(self._process_message(message), loop=self.loop)
+        coroutine = self._process_message(message)
+        task = asyncio.ensure_future(coroutine, loop=self.loop)
+
+        self.tasks.add(task)
 
     async def shutdown(self):
-        pass
+        """ Waits for all pending tasks and shuts down the stages """
+        shutdown_tasks = [stage.shutdown() for stage in self.stages]
+
+        while self.tasks:
+            self.tasks, pending = [], self.tasks
+
+            await asyncio.gather(pending,
+                loop=self.loop, return_exceptions=True)
+
+        await asyncio.gather(shutdown_tasks,
+            loop=self.loop, return_exceptions=True)
