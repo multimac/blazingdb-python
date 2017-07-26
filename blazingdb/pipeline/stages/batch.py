cfe87a263e01491501c881df11fc0ae848071695
David Symons <david.symons@onemodel.co>
2017-07-26 13:06:39 -0500

    Rework BatchStage to handle DataFramePackets

--- blazingdb/pipeline/stages/batch.py
+++ blazingdb/pipeline/stages/batch.py
@@ -2,12 +2,7 @@
 Defines the base batcher class for generating batches of data to load into BlazingDB
 """
 
-import io
-import logging
-
-import aiofiles
-
-from blazingdb.util import format_size, timer
+import pandas
 
 from . import base
 from .. import packets
@@ -20,58 +15,41 @@ class BatchStage(base.BaseStage):
 
     DEFAULT_LOG_INTERVAL = 10
 
-    def __init__(self, batch_size, loop=None, **kwargs):
+    def __init__(self, batch_size):
         super(BatchStage, self).__init__(packets.DataFilePacket, packets.DataCompletePacket)
-        self.logger = logging.getLogger(__name__)
-
-        self.generators = dict()
-        self.loop = loop
-
         self.batch_size = batch_size
-        self.log_interval = kwargs.get("log_interval", BatchStage.DEFAULT_LOG_INTERVAL)
+        self.generators = dict()
 
-    async def _generate_batch(self):
-        def _log_progress(stream):
-            read_size = format_size(stream.tell())
-            limit_size = format_size(self.batch_size)
-
-            self.logger.info("Read %s of %s byte(s)", read_size, limit_size)
+    def _generate_batch(self):
+        frame_data = yield
 
         index = 0
-        data_file = None
         while True:
-            stream = io.StringIO(newline='')
+            while True:
+                memory_usage = frame_data.memory_usage().sum()
+                row_count = frame_data.shape()[0]
 
-            while stream.tell() < self.batch_size:
-                log_timer = timer.RepeatedTimer(self.log_interval,
-                    _log_progress, stream, loop=self.loop)
+                if memory_usage < self.batch_size:
+                    break
 
-                with log_timer:
-                    if data_file is None:
-                        data_args = yield
+                batch_rows = (self.batch_size / memory_usage) * row_count
 
-                        if data_args is None:
-                            if stream.tell() > 0:
-                                yield stream, index
+                batch_frame = frame_data.iloc[:batch_rows]
+                frame_data = frame_data.iloc[batch_rows:]
 
-                            return
+                yield batch_frame, index
 
-                        data_file_path, format_pkt = data_args
-                        data_file = await aiofiles.open(data_file_path,
-                            newline=format_pkt.line_terminator)
+                index += 1
 
-                    read_amount = self.batch_size - stream.tell()
-                    lines = await data_file.readlines(read_amount)
+            next_frame = yield
 
-                    if not lines:
-                        data_file = None
-                        continue
+            if next_frame is None:
+                break
 
-                    stream.writelines(lines)
+            frame_data = pandas.concat(
+                [frame_data, next_frame])
 
-            yield stream, index
-
-            index += 1
+        yield batch_frame, index
 
     def _get_generator(self, msg_id):
         if msg_id in self.generators:
@@ -93,29 +71,32 @@ class BatchStage(base.BaseStage):
     async def process(self, message):
         """ Generates a series of batches from the stream """
         generator = self._get_generator(message.initial_id)
-        format_pkt = message.get_packet(packets.DataFormatPacket)
 
-        load_packets = []
-        for packet in message.pop_packets(packets.DataFilePacket):
-            batch = await generator.asend(packet.file_path, format_pkt)
+        frame_packets = []
+        for packet in message.pop_packets(packets.DataFramePacket):
+            batch = await generator.send(packet.frame)
 
             while batch is not None:
-                stream, index = batch
-                load_packet = packets.DataLoadPacket(stream, index)
-                load_packets.append(load_packet)
+                frame, index = batch
 
-                batch = await generator.asend(None)
+                frame_packet = packets.DataFramePacket(frame, index)
+                frame_packets.append(frame_packet)
 
-        complete_packet = message.get_packet(packets.DataCompletePacket, default=None)
+                batch = await generator.send(None)
+
+        complete_packet = message.get_packet(
+            packets.DataCompletePacket, default=None)
+
         if complete_packet is not None:
-            batch = await generator.asend(None)
+            batch = await generator.send(None)
 
             if batch:
-                stream, index = batch
-                load_packet = packets.DataLoadPacket(stream, index)
-                load_packets.append(load_packet)
+                frame, index = batch
+
+                frame_packet = packets.DataFramePacket(frame, index)
+                frame_packets.append(frame_packet)
 
             self._delete_generator(message.msg_id)
 
-        if load_packets:
-            await message.forward(*load_packets)
+        if frame_packets:
+            await message.forward(*frame_packets)
