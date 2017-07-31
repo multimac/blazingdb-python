5ea81bceb281bf106870fe91de3eb86e29a18e65
David Symons <david.symons@onemodel.co>
2017-07-31 10:05:14 -0500

    Rename importers module to load

--- blazingdb/pipeline/stages/__init__.py
+++ blazingdb/pipeline/stages/__init__.py
@@ -7,9 +7,9 @@ from .base import BaseStage, PipelineStage, When
 from .batch import BatchStage
 from .custom import CustomActionStage
 from .database import CreateTableStage, DropTableStage, SourceComparisonStage, TruncateTableStage
-from .importers import FileImportStage, FileOutputStage
+from .load import FileImportStage, FileOutputStage
 from .misc import DelayStage, InjectPacketStage, PromptInputStage, SingleFileStage, SkipTableStage
 from .unload import UnloadGenerationStage, UnloadRetrievalStage
 
 
-__all__ = ["base", "batch", "custom", "database", "importers", "misc", "unload"]
+__all__ = ["base", "batch", "custom", "database", "load", "misc", "unload"]
